ca0d8613f8e44406977836015b71bb29e1fd3ef3
toiitdotcom <www.toiit.com@gmail.com>
2017-03-12 01:28:17 +0700

    update file constant.php

--- constant.php
+++ constant.php
@@ -1,4 +1,4 @@
 <?php
+defined('BASEPATH') || define('BASEPATH', realpath(dirname(__FILE__)));
 define('ENVIRONMENT', 'dev');
 define('USEDB', 'yes');
-defined('BASEPATH') || define('BASEPATH', realpath(dirname(__FILE__)));
